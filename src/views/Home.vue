4bf62f2e1bccd990dc1ea6d807edbc028c958aab
Kay0s <hamilton.kristina@gmail.com>
2021-04-25 16:27:39 -0500

    required hourglass.gif in Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,39 @@
 <template>
-  <div class="home">
-    Hello World
-  </div>
+  <main v-if="!loading">
+    Show Data
+  </main>
+
+  <main class="flex flex-col align-center justify-center text-center" v-else></main>
 </template>
 
 <script>
-
 export default {
   name: 'Home',
-  components: {
+  components: {},
+  data() {
+    return {
+      loading: true,
+      title: 'Global',
+      dataDate: '',
+      stats: {},
+      countries: [],
+      loadingImage: require('../assets/hourglass.gif')
+    }
+  },
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch ('https://api.covid19api.com/summary');
+      const data = await res.json();
+      return data;
+    }
+  },
+  async created() {
+    const data = await this.fetchCovidData();
     
-  }
+    this.dataDate = data.Date
+    this.stats = data.Global
+    this.countries = data.Countries
+    this.loading =  false
+  },
 }
 </script>
