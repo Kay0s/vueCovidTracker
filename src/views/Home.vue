e5b0d035f56b71e13b40955437ffa477fcbcf5a3
Kay0s <hamilton.kristina@gmail.com>
2021-04-25 17:04:37 -0500

    DataTitle component created and added to Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,24 @@
 <template>
   <main v-if="!loading">
-    Show Data
+    <DataTitle :text="title" :dataDate="dataDate" />
   </main>
 
-  <main class="flex flex-col align-center justify-center text-center" v-else></main>
+  <main class="flex flex-col align-center justify-center text-center" v-else>
+    <div class="text-gray-500 text-3xl mt-10 mb-6">
+      Fetching Data
+    </div>
+    <img :src="loadingImage" class="w-24 m-auto" alt="hourglass">
+  </main>
 </template>
 
 <script>
+import DataTitle from '@/components/DataTitle'
+
 export default {
   name: 'Home',
-  components: {},
+  components: {
+    DataTitle
+  },
   data() {
     return {
       loading: true,
